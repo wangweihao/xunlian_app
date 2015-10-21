f1d13604404bb3a406d20222b03d971c58f20f2c
wwh <578867817@qq.com>
2015-10-21 16:48:57 +0800

    通过map将mark转化为对应类名--单元测试通过

--- src/main/java/com/wangweihao/Object/MarkToClassObjectMap.java
+++ src/main/java/com/wangweihao/Object/MarkToClassObjectMap.java
@@ -9,9 +9,16 @@ import java.util.HashMap;
  */
 public class MarkToClassObjectMap {
     /* 第一层括号为匿名内部类，第二层括号为实例初始化块 */
-    HashMap<Integer, String> markToClass = new HashMap<Integer, String>(){
-        {
-            put(1, "DectAccountRegister");
-        }
-    };
+    public MarkToClassObjectMap() {
+        markToClass = new HashMap<Integer, String>() {
+            {
+                put(1, "com.wangweihao.Object.DectAccountRegister");
+            }
+        };
+    }
+    public String Mapping(int _mark){
+        return markToClass.get(_mark);
+    }
+
+    HashMap<Integer, String> markToClass;
 }
