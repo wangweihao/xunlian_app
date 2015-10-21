bd76f19c1f74182fb6081901510424d3bc6c3078
wwh <578867817@qq.com>
2015-10-22 06:29:03 +0800

    成功编码返回(去除重复数据)--单元测试通过

--- src/main/java/com/wangweihao/AccessDatabase/AccessDatabase.java
+++ src/main/java/com/wangweihao/AccessDatabase/AccessDatabase.java
@@ -1,6 +1,11 @@
 package com.wangweihao.AccessDatabase;
 
 import com.wangweihao.Object.RecvBasicMessageObject;
+import com.wangweihao.Xl_db.DatabasePool;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 /**
  * Created by wwh on 15-10-21.
@@ -9,13 +14,14 @@ public class AccessDatabase {
     public AccessDatabase(){
         basicObject = new RecvBasicMessageObject();
         sqlString = new String();
+        DBPoolConnection = DatabasePool.getConnection();
     }
     public AccessDatabase(int _mark, String _account){
         basicObject = new RecvBasicMessageObject(_mark, _account);
         sqlString = new String();
     }
 
-    public RecvBasicMessageObject AccessXlDatabase(){
+    public RecvBasicMessageObject AccessXlDatabase() throws SQLException {
         return null;
     }
 
@@ -25,4 +31,6 @@ public class AccessDatabase {
 
     public RecvBasicMessageObject basicObject;
     public String sqlString;
+    public Connection DBPoolConnection;
+    public PreparedStatement preparedStatement;
 }
--- src/main/java/com/wangweihao/Object/AccountRegister.java
+++ src/main/java/com/wangweihao/Object/AccountRegister.java
@@ -2,6 +2,9 @@ package com.wangweihao.Object;
 
 import com.wangweihao.AccessDatabase.AccessDatabase;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  * Created by wwh on 15-10-21.
  */
@@ -14,8 +17,19 @@ public class AccountRegister extends AccessDatabase {
     }
 
     @Override
-    public RecvBasicMessageObject AccessXlDatabase(){
+    public RecvBasicMessageObject AccessXlDatabase() throws SQLException {
         System.out.println("帐号注册");
+        sqlString = "select count(*) from UserInfo where account = \"" +
+                basicObject.getAccount() + "\";";
+        preparedStatement = DBPoolConnection.prepareStatement(sqlString);
+        ResultSet resultset = preparedStatement.executeQuery();
+        resultset.next();
+        int result = resultset.getInt(1);
+        if(result == 1){
+            System.out.println("该帐号存在");
+        }else {
+            System.out.println("该帐号不存在");
+        }
         return basicObject;
     }
 
