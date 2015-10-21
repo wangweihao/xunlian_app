15c2b936a1ba6e3252fc3974b5a378e3042c0e14
wwh <578867817@qq.com>
2015-10-21 10:56:14 +0800

    完成基本对象设计--单元测试通过

---
+++ src/main/java/com/wangweihao/Object/DecectAccountRegister.java
@@ -0,0 +1,10 @@
+package com.wangweihao.Object;
+
+/**
+ * Created by wwh on 15-10-21.
+ */
+public class DecectAccountRegister extends ServerRecvMessageObject {
+    DecectAccountRegister(int _mark, String _account){
+        super(_mark, _account);
+    }
+}
