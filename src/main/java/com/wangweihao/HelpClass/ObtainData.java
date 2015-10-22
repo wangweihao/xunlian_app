940c85efccb51c91ba4f01812a4b50dcc104dd0e
wwh <578867817@qq.com>
2015-10-22 16:06:00 +0800

    获取本地时间类--单元测试通过

---
+++ src/main/java/com/wangweihao/HelpClass/ObtainData.java
@@ -0,0 +1,24 @@
+package com.wangweihao.HelpClass;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by wwh on 15-10-22.
+ */
+public class ObtainData {
+    private static Date date;
+    private static DateFormat format;
+    static {
+        date = new Date();
+        format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    }
+    public static String getData(){
+        return format.format(date);
+    }
+
+    public static void main(String[] args){
+        System.out.println(getData());;
+    }
+}
