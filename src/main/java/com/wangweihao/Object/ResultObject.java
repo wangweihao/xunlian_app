002555fbdb19835a6afd36e40d1b88fd6ff9073b
wwh <578867817@qq.com>
2015-10-27 12:56:40 +0800

    构造返回值对象

---
+++ src/main/java/com/wangweihao/Object/ResultObject.java
@@ -0,0 +1,115 @@
+package com.wangweihao.Object;
+
+/**
+ * Created by wwh on 15-10-27.
+ */
+public class ResultObject {
+    private String name;
+    private byte[] head;
+    private String personNumber;
+    private String workPhoneNumber;
+    private String homePhoneNumber;
+    private String personEmail;
+    private String workEmail;
+    private String homeEmail;
+    private String qqNumber;
+    private String weiboNumber;
+    int error;
+    private String status;
+
+    public String getName() {
+        return name;
+    }
+
+    public byte[] getHead() {
+        return head;
+    }
+
+    public String getPersonNumber() {
+        return personNumber;
+    }
+
+    public String getWorkPhoneNumber() {
+        return workPhoneNumber;
+    }
+
+    public String getHomePhoneNumber() {
+        return homePhoneNumber;
+    }
+
+    public String getPersonEmail() {
+        return personEmail;
+    }
+
+    public String getWorkEmail() {
+        return workEmail;
+    }
+
+    public String getHomeEmail() {
+        return homeEmail;
+    }
+
+    public String getQqNumber() {
+        return qqNumber;
+    }
+
+    public String getWeiboNumber() {
+        return weiboNumber;
+    }
+
+    public int getError() {
+        return error;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHead(byte[] head) {
+        this.head = head;
+    }
+
+    public void setPersonNumber(String personNumber) {
+        this.personNumber = personNumber;
+    }
+
+    public void setWorkPhoneNumber(String workPhoneNumber) {
+        this.workPhoneNumber = workPhoneNumber;
+    }
+
+    public void setHomePhoneNumber(String homePhoneNumber) {
+        this.homePhoneNumber = homePhoneNumber;
+    }
+
+    public void setPersonEmail(String personEmail) {
+        this.personEmail = personEmail;
+    }
+
+    public void setWorkEmail(String workEmail) {
+        this.workEmail = workEmail;
+    }
+
+    public void setHomeEmail(String homeEmail) {
+        this.homeEmail = homeEmail;
+    }
+
+    public void setQqNumber(String qqNumber) {
+        this.qqNumber = qqNumber;
+    }
+
+    public void setWeiboNumber(String weiboNumber) {
+        this.weiboNumber = weiboNumber;
+    }
+
+    public void setError(int error) {
+        this.error = error;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
