d2d0b2055c993b8f3bf53b1109734efd9984a9ce
wwh <578867817@qq.com>
2015-12-17 22:12:40 +0800

    初步完成 ets 存储用户数据

--- IMServer/src/im_server_mapper.erl
+++ IMServer/src/im_server_mapper.erl
@@ -7,6 +7,7 @@
 %% ------------------------------------------------------------------
 
 -export([start_link/0]).
+-export([lookup/2, insert/3]).
 
 %% ------------------------------------------------------------------
 %% gen_server Function Exports
@@ -15,20 +16,51 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
+-record(state, {
+            mapid
+         }).
+
 %% ------------------------------------------------------------------
 %% API Function Definitions
 %% ------------------------------------------------------------------
 
 start_link() ->
-    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [#state{}], []).
+
+lookup(Account, Pid) ->
+    gen_server:call(?MODULE, {lookup, Account, Pid}).
+
+insert(Account, Socket, Pid) ->
+    gen_server:call(?MODULE, {insert, Account, Socket, Pid}).
 
 %% ------------------------------------------------------------------
 %% gen_server Function Definitions
 %% ------------------------------------------------------------------
 
-init(Args) ->
+init(State) ->
     MapId = ets:new(mapper, [set]),
-    {ok, Args}.
+    State1 = State#state{
+                mapid = MapId
+              },
+    {ok, State1}.
+
+handle_call({lookup, Account, Pid}, _From, State) ->
+    case (catch ets:lookup(State#state.mapid, Account)) of
+        [{_, Socket}] ->
+            Pid ! {Socket};
+        _  ->
+            Pid ! {error}
+    end,
+    {reply, ok, State};
+
+handle_call({insert, Account, Socket, Pid}, _From, State) ->
+    case (catch ets:insert(State#state.mapid, {Account, Socket})) of
+        true ->
+            Pid ! {ok};
+        _ ->
+            Pid ! {error}
+    end,
+    {reply, ok, State};
 
 handle_call(_Request, _From, State) ->
     {reply, ok, State}.
