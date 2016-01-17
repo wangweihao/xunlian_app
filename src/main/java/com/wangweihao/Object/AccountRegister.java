5e9df6644c695c3ce1858cf669227f21173e89b4
wwh <578867817@qq.com>
2016-01-17 22:22:31 +0800

    完成通过验证密宝修改用户密码 -- 单元测试通过

--- src/main/java/com/wangweihao/Object/AccountRegister.java
+++ src/main/java/com/wangweihao/Object/AccountRegister.java
@@ -47,14 +47,14 @@ public class AccountRegister extends AccessDatabase {
     public void setDerivedClassOtherMeber(){
         jsonObject = new JSONObject(RequestString);
         flag = jsonObject.getInt("flag");
-        secret = new String(jsonObject.getString("secret"));
+        password = new String(jsonObject.getString("secret"));
         question = new String(jsonObject.getString("question"));
         answer = new String(jsonObject.getString("answer"));
     }
 
     public void accountRegister() throws SQLException {
         String accountRegisterSql = "insert into UserInfo (account, password, question, answer) values (\"" +
-                basicObject.getAccount() + "\", \"" + secret + "\", \"" + question + "\", \"" + answer + "\");";
+                basicObject.getAccount() + "\", \"" + password + "\", \"" + question + "\", \"" + answer + "\");";
         preparedStatement = DBPoolConnection.prepareStatement(accountRegisterSql);
         try {
             preparedStatement.executeUpdate();
@@ -74,12 +74,16 @@ public class AccountRegister extends AccessDatabase {
             resultSet.next();
         } catch (SQLException e) {
             e.printStackTrace();
-            buildReturnValue(1, "success", "failure", "密码错误，请重试");
+            buildReturnValue(1, "success", "failure", "系统错误，请重试");
             return;
         }
         String UserSecret = resultSet.getString(1);
-        if(UserSecret == secret){
+        if(UserSecret.equals(password)){
             System.out.println("验证密码成功");
+        }else{
+            System.out.println("验证密码错误");
+            buildReturnValue(1, "success", "failure", "验证密码错误，请稍后再试");
+            return;
         }
         String updateQuestionSql = "update UserInfo set question = \"" + question + "\", answer = \"" +
                 answer + "\" where account = \"" + basicObject.getAccount() + "\";";
@@ -94,7 +98,38 @@ public class AccountRegister extends AccessDatabase {
         buildReturnValue(0, "success", "success", "更新密宝问题成功");
     }
 
-    public void modifySecretByQuestion(){
+    public void modifySecretByQuestion() throws SQLException{
+        String getUserAnswerSql = "select answer from UserInfo where account = \"" + basicObject.getAccount()
+                + "\";";
+        try{
+            preparedStatement = DBPoolConnection.prepareStatement(getUserAnswerSql);
+            resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            buildReturnValue(1, "success", "failure", "系统错误，请重试");
+            return;
+        }
+        String UserAnswer = resultSet.getString(1);
+        if(UserAnswer.equals(answer)){
+            System.out.println("验证密宝成功");
+        }else{
+            System.out.println(UserAnswer);
+            System.out.println(answer);
+            buildReturnValue(1, "success", "failure", "密宝回答错误，请重新输入");
+            return;
+        }
+        String updateUserPassword = "update UserInfo set password = \"" + password + "\" where account = \"" +
+                basicObject.getAccount() + "\";";
+        try{
+            preparedStatement = DBPoolConnection.prepareStatement(updateUserPassword);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e){
+            e.printStackTrace();
+            buildReturnValue(1, "success", "failure", "系统错误，请稍后再试");
+            return;
+        }
+        buildReturnValue(0, "success", "success", "验证密宝通过，修改密码成功");
 
     }
 
@@ -129,7 +164,7 @@ public class AccountRegister extends AccessDatabase {
 
     private int flag;
     private int uid;
-    private String secret;
+    private String password;
     private String question;
     private String answer;
     private ResultSet resultSet;
