b7a22af8dfab5cd369cb78f2a247ba7b06021e8d
wwh <578867817@qq.com>
2016-01-17 22:37:43 +0800

    获取用户密宝问题 -- 单元测试通过

---
+++ src/main/java/com/wangweihao/Object/ObtainQuestion.java
@@ -0,0 +1,48 @@
+package com.wangweihao.Object;
+
+import com.wangweihao.AccessDatabase.AccessDatabase;
+import com.wangweihao.HelpClass.ObtainData;
+import org.json.JSONObject;
+
+import javax.lang.model.element.TypeElement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created by wwh on 15-10-21.
+ */
+public class ObtainQuestion extends AccessDatabase{
+    public ObtainQuestion(){
+        super();
+    }
+
+    public ObtainQuestion(int _mark, String _account){
+        super(_mark, _account);
+    }
+
+    @Override
+    public AccessDatabase AccessXlDatabase() throws SQLException {
+        String obtainQuestionSql = "select question from UserInfo where account = \"" + basicObject.getAccount()
+                + "\";";
+        preparedStatement = DBPoolConnection.prepareStatement(obtainQuestionSql);
+        try{
+            resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+        } catch (SQLException e){
+            e.printStackTrace();
+            buildReturnValue(1, "success", "failure", "系统错误，请稍后再试");
+            return this;
+        }
+        question = resultSet.getString(1);
+        buildReturnValue(1, "success", "success", question);
+        return this;
+    }
+
+    @Override
+    public void setDerivedClassOtherMeber(){
+
+    }
+
+    private ResultSet resultSet;
+    private String question;
+}
