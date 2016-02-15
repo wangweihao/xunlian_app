3458b2c262cebb7525bc384a34d0479dfdba99b0
wwh <578867817@qq.com>
2016-02-15 17:01:33 +0800

    修复编码器 bug -- 单元测试通过

--- src/main/java/com/wangweihao/Xl_handler/XlServerInitializer.java
+++ src/main/java/com/wangweihao/Xl_handler/XlServerInitializer.java
@@ -1,8 +1,10 @@
 package com.wangweihao.Xl_handler;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelInitializer;
-import io.netty.channel.Channel;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
 
 /**
  * Created by wwh on 15-10-18.
@@ -10,7 +12,10 @@ import io.netty.channel.socket.SocketChannel;
 public class XlServerInitializer extends ChannelInitializer<SocketChannel>{
     @Override
     public void initChannel(SocketChannel socketChannel) throws Exception{
-        ChannelPipeline pipeline = socketChannel.pipeline();
-        pipeline.addLast(new XlServerHandler());
+        ByteBuf delimiter = Unpooled.copiedBuffer("\r\n".getBytes());
+        socketChannel.pipeline().addLast(
+                new DelimiterBasedFrameDecoder(10240,
+                        delimiter));
+        socketChannel.pipeline().addLast(new XlServerHandler());
     }
 }
